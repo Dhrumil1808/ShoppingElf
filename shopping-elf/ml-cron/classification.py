d5870ab9a5326381e8807c8cc56e68bda5b29438
rashmishrm <rashmishrm74@gmail.com>
2017-04-19 15:47:17 -0700

    Buying_pattern ml tryouts

--- shopping-elf/ml-cron/classification.py
+++ shopping-elf/ml-cron/classification.py
@@ -2,6 +2,35 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import BernoulliNB
 
 
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+with open("data-set.txt","r") as text_file:
+    lines = text_file.read().split("\n")
+
+lines= [line.split("|") for line in lines if ((len(line.split("|"))==2) and (line.split("|")[1]<> ''))]
+training_documents = [line[0] for line in lines]
+#print training_documents
+training_labels = [line[1] for line in lines]
+#print  training_labels
+# fit a k-nearest neighbor model to the data
+model = KNeighborsClassifier()
+count_vectorizer = CountVectorizer(binary='true')
+
+training_documents = count_vectorizer.fit_transform(training_documents)
+
+model.fit(training_documents, training_labels)
+
+
+print(model)
+# make predictions
+expected = "1"
+predicted = model.predict(count_vectorizer.transform(["bread"]));
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
+
+
 with open("data-set.txt","r") as text_file:
     lines = text_file.read().split("\n")
 
