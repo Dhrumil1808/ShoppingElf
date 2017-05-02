5e933063bd2735fe31ecfd30f79f0d8eb5061453
rashmishrm <rashmishrm74@gmail.com>
2017-05-02 02:14:20 -0700

    nltk sample tryouts for product clustering

---
+++ shopping-elf/ml-cron/Product_Clustering.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import nltk
+import re
+import os
+import codecs
+from sklearn import feature_extraction
+import mpld3
+
+
+
+# load nltk's English stopwords as variable called 'stopwords'
+stopwords = nltk.corpus.stopwords.words('english')
+
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("english")
+
+
+def tokenize_and_stem(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
+
+
+def tokenize_only(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    return filtered_tokens
+
+
+#not super pythonic, no, not at all.
+#use extend so it's a big flat list of vocab
+totalvocab_stemmed = []
+totalvocab_tokenized = []
+with open("data/product-data.txt","r") as text_file:
+    lines = text_file.read().split("\n")
+for i in lines:
+    allwords_stemmed = tokenize_and_stem(i) #for each item in 'synopses', tokenize/stem
+    totalvocab_stemmed.extend(allwords_stemmed) #extend the 'totalvocab_stemmed' list
+
+    allwords_tokenized = tokenize_only(i)
+    totalvocab_tokenized.extend(allwords_tokenized)
+
+vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index = totalvocab_stemmed)
+print 'there are ' + str(vocab_frame.shape[0]) + ' items in vocab_frame'
+
+print vocab_frame.head()
+print
+print
+print
+print
