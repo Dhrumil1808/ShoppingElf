953f4a18a0a8e803aecdf3ad3df794d22834cc94
Aditya Parmar <parmar415@gmail.com>
2017-05-08 19:54:35 -0700

    Create demo model to learn

---
+++ shopping-elf/Android-APP/ShoppingElf_v_1.0/app/src/main/java/com/example/adityaparmar/shoppingelf_v_10/model/GitHubRepo.java
@@ -0,0 +1,21 @@
+package com.example.adityaparmar.shoppingelf_v_10.model;
+
+/**
+ * Created by adityaparmar on 5/8/17.
+ */
+
+public class GitHubRepo {
+    private int id;
+    private String name;
+
+    public GitHubRepo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
