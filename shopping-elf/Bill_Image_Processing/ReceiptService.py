098bd7d88462436a949bb7bfb570f75dc14f8261
Neil Thaker <neilthaker07@gmail.com>
2017-05-08 19:46:54 -0700

    Query changes for mysql

--- shopping-elf/Bill_Image_Processing/ReceiptService.py
+++ shopping-elf/Bill_Image_Processing/ReceiptService.py
@@ -3,9 +3,11 @@ from time import gmtime, strftime
 from Models import BillReceipt
 from Models import BillItem
 from Models import User
+import mysql.connector
 
-cluster = Cluster()
-session = cluster.connect('shopping_elf')
+# cluster = Cluster()
+# session = cluster.connect('shopping_elf')
+database=None
 
 def addUserReciept(billReceipt):
     bill_date= billReceipt.billDate;
@@ -14,35 +16,54 @@ def addUserReciept(billReceipt):
     print user_id
     print family_members
     currentDate= strftime("%Y-%m-%d", gmtime())
-
+    
+    self.database = mysql.connector.connect(user=DbConstants.USER, passwd=DbConstants.PASSWORD, host=DbConstants.HOST, database=DbConstants.DATABASE)
+    cursor = self.database.cursor()
     for eachItem in billReceipt.billItems:
-        session.execute(
-        """
-        INSERT INTO receipt_data (userid, bill_date, qty,
-        time, product_name, family_members) VALUES( %(userId)s, %(bill_date)s, %(qty)s , %(time)s,%(product_name)s , %(family_members)s)
-        """,
-        {"userId": user_id, "bill_date": bill_date,'qty': eachItem.quantity,'time': currentDate, 'product_name': eachItem.productName,'family_members':family_members}
-        )
+        query = """INSERT INTO receipt_data (userid,bill_date,qty,time, product_name, family_members) VALUES (%s,%s,%s,%s,%s,%s)"""
+        cursor.execute(query, (self.user_id,self.bill_date, self.qty, self.time, self.product_name, self.family_members))
+        self.database.commit()
+        cursor.close()
+        self.database.close()
+        return result
 
 def addProducts(products):
-    for eachProduct in products:
-        session.execute(
-        """
-        INSERT INTO products (product_name) VALUES( %(product_name)s)
-        """,
-        {"product_name": eachProduct})
+    self.database = mysql.connector.connect(user=DbConstants.USER, passwd=DbConstants.PASSWORD, host=DbConstants.HOST, database=DbConstants.DATABASE)
+    cursor = self.database.cursor()
 
+    for eachProduct in products:
+        query = """INSERT INTO products (product_name) VALUES (%s)"""
+        cursor.execute(query, (self.eachProduct))
+        self.database.commit()
+        cursor.close()
+        self.database.close()
+        return result
 
 def fetchAllReciepts(userId):
-    query = "SELECT product_name,bill_date,qty FROM receipt_data where userid=\'" + str(userId)+"\' order by product_name,bill_date asc;";
-    rows = session.execute(query)
+    self.database = mysql.connector.connect(user=DbConstants.USER, passwd=DbConstants.PASSWORD, host=DbConstants.HOST, database=DbConstants.DATABASE)
+    cursor = self.database.cursor()
+    try:
+        query = """SELECT product_name,bill_date,qty FROM receipt_data where userid = %s order by product_name,bill_date asc"""
+        cursor.execute(query,(self.userid))
+        rows = cursor.fetchall()
+    except mysql.connector.Error as err:
+        cursor.close()
+        self.database.close()
+        return "err"
     for user_row in rows:
         print user_row.bill_date, user_row.product_name, user_row.qty
 
-
 def findUser(username):
-    query = "SELECT userid,family_members,username  FROM user where username=\'" + str(username)+"\'";
-    rows = session.execute(query)
+    self.database = mysql.connector.connect(user=DbConstants.USER, passwd=DbConstants.PASSWORD, host=DbConstants.HOST, database=DbConstants.DATABASE)
+    cursor = self.database.cursor()
+    try:
+        query = """SELECT userid,family_members,username  FROM user where username= %s """
+        cursor.execute(query,(self.userid))
+        rows = cursor.fetchall()
+    except mysql.connector.Error as err:
+        cursor.close()
+        self.database.close()
+        return "err"
     for user_row in rows:
         user = User(user_row.userid,user_row.username,user_row.family_members);
     return user;
