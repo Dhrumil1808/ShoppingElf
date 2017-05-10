ab70acb8c7db585e55e8e89b3b250f28adc4735a
Aditya Parmar <parmar415@gmail.com>
2017-05-10 02:03:11 -0700

    Deleted

--- shopping-elf/Android-APP/ShoppingElf_v_1.0/app/src/main/java/com/example/adityaparmar/shoppingelf_v_10/PlaceholderFragment.java
+++ shopping-elf/Android-APP/ShoppingElf_v_1.0/app/src/main/java/com/example/adityaparmar/shoppingelf_v_10/PlaceholderFragment.java
@@ -7,13 +7,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.example.adityaparmar.shoppingelf_v_10.model.MyModel;
+import com.example.adityaparmar.shoppingelf_v_10.adapter.MyCardsAdapter;
+import com.example.adityaparmar.shoppingelf_v_10.model.MyCards;
+import com.example.adityaparmar.shoppingelf_v_10.service.CardsClient;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import com.example.adityaparmar.shoppingelf_v_10.adapter.MyAdapter;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by adityaparmar on 5/7/17.
@@ -24,6 +31,8 @@ public  class PlaceholderFragment extends Fragment {
 
 
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private List<MyCards> glist;
+    RecyclerView rv;
 
     public PlaceholderFragment() {
     }
@@ -56,28 +65,14 @@ public  class PlaceholderFragment extends Fragment {
             }
             case 2: {
                 rootView = inflater.inflate(R.layout.fragment_shopping_card, container, false);
-                RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rc_grocery_card);
+                rv = (RecyclerView) rootView.findViewById(R.id.rc_grocery_card);
                 rv.setHasFixedSize(true);
-                   /* MyModel[] data = new MyModel[2];
-                    data[0].setItems("I am first");
-                    data[1].setItems("I am first");
-                    */
-                List<MyModel> list = new ArrayList<>();
-                MyModel data;
-                for(int i=0;i<5;i++)
-                {
-                    data = new MyModel(
-                            i,"hello","hello","hello"
-
-                    );
-                    list.add(data);
-                }
 
 
 
-                MyAdapter adapter = new MyAdapter(list);
+                feedData();
 
-                rv.setAdapter(adapter);
+
 
                 LinearLayoutManager llm = new LinearLayoutManager(getActivity());
                 rv.setLayoutManager(llm);
@@ -87,6 +82,50 @@ public  class PlaceholderFragment extends Fragment {
 
         }
         return rootView;
+    }
+
+    public void feedData()
+    {
+        String API_BASE_URL = "http://54.241.140.236:3008/";
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl(API_BASE_URL)
+                .addConverterFactory(
+                        GsonConverterFactory.create()
+                );
+
+        Retrofit retrofit = builder.client(httpClient.build()).build();
+
+
+
+        CardsClient client =  retrofit.create(CardsClient.class);
+
+        // Fetch a list of the Github repositories.
+        Call<List<MyCards>> call = client.getcards();
+
+        // Execute the call asynchronously. Get a positive or negative callback.
+        call.enqueue(new Callback<List<MyCards>>() {
+            @Override
+            public void onResponse(Call<List<MyCards>> call, Response<List<MyCards>> response) {
+                // The network call was a success and we got a response
+                // TODO: use the repository list and display it
+                glist = response.body();
+                MyCardsAdapter adapter = new MyCardsAdapter(glist);
+                rv.setAdapter(adapter);
+
+                Toast.makeText(getActivity(),response.body().get(1).getProductName(),Toast.LENGTH_LONG).show();
+               //listView.setAdapter(new GitHubRepoAdapter(Recto.this,respo));
+               // Toast.makeText(Recto.this,"positive responce", Toast.LENGTH_LONG);
+            }
+
+            @Override
+            public void onFailure(Call<List<MyCards>> call, Throwable t) {
+                // the network call was a failure
+                // TODO: handle error
+            }
+        });
     }
 
 
