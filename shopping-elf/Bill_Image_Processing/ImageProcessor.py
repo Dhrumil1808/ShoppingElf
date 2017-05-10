1db8c705e9c726e11b9df07c23fac230d13a10d6
Neil Thaker <neilthaker07@gmail.com>
2017-05-09 17:33:39 -0700

    Logical changes in parsing words

--- shopping-elf/Bill_Image_Processing/ImageProcessor.py
+++ shopping-elf/Bill_Image_Processing/ImageProcessor.py
@@ -30,25 +30,38 @@ class ImageProcessor:
 		#print originalString
 		items = [];
 		groupBynewLine = re.split(r'[\n\r]+', originalString)
-		del groupBynewLine[0]
-		del groupBynewLine[0]
-		del groupBynewLine[0]
+		# del groupBynewLine[0]
+		# del groupBynewLine[0]
+		# del groupBynewLine[0]
+
+		print groupBynewLine
 
 		dict1 = {}
 		dict2 = {}
 		counter = 0
+		support = 1
 		for i in groupBynewLine:
-			if not re.search(r"(\d+)", i):
-				dict1[counter]=i
-			else:
-				dict2[counter-1]=i
-			# print i
-			counter+=1
+			if len(i) > 0:
+				if not i[0].isdigit():
+					#i_update = ''.join(e for e in i if e.isalnum())
+					i_update = re.sub(r'([^\s\w]|_)+', '', i)
+					if i_update:
+						dict1[counter]=i_update
+						support = 1
+					else:
+						support = 0
+				else:
+					dict2[counter-1]=i
+					support = 1
 
-		# print '1111111'
-		# print dict1
-		# print '2222222'
-		# print dict2
+			if support == 1:
+				counter+=1
+			# if not re.search(r"(\d+)", i):
+			
+		print '1111111'
+		print dict1
+		print '2222222'
+		print dict2
 
 		dc = {}
 		for i,j in dict1.items():
@@ -64,12 +77,15 @@ class ImageProcessor:
 		# print '3333333'
 		# print dc
 
-		for i1,j1 in dc.items():
-			if isinstance(j1, int):
-				dc[i1]=j1
-			else:
-				k1=shlex.split(j1)[0]
-				dc[i1]=k1
+		try:
+			for i1,j1 in dc.items():
+				if isinstance(j1, int):
+					dc[i1]=j1
+				else:
+					k1=shlex.split(j1)[0]
+					dc[i1]=k1
+		except:
+			return 'please correct input image'
 
 		#create bill receipt_data
 		try:
